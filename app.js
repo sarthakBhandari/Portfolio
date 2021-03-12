fd717062bdf67b34cc412e2598d317046fedc301
Sarthak <79257161+sarthakBhandari@users.noreply.github.com>
2021-03-12 23:47:31 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+const hamburger = document.querySelector(
+  ".header .nav-bar .nav-list .hamburger"
+);
+const mobile_menu = document.querySelector(".header .nav-bar .nav-list ul");
+const menu_item = document.querySelectorAll(
+  ".header .nav-bar .nav-list ul li a"
+);
+const header = document.querySelector(".header.container");
+const galactica = document.getElementById("Galactica");
+const calculator = document.getElementById("Calculator");
+const ticTac = document.getElementById("TicTac");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  mobile_menu.classList.toggle("active");
+});
+
+document.addEventListener("scroll", () => {
+  var scroll_position = window.scrollY;
+  if (scroll_position > 250) {
+    header.style.backgroundColor = "#29323c";
+  } else {
+    header.style.backgroundColor = "transparent";
+  }
+});
+
+menu_item.forEach((item) => {
+  item.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    mobile_menu.classList.toggle("active");
+  });
+});
+
+galactica.addEventListener("click", () => {
+  window.open(
+    "https://sarthakbhandari.github.io/Shooting-Star-Galactica/",
+    "_blank"
+  );
+});
+calculator.addEventListener("click", () => {
+  window.open(
+    "https://sarthakbhandari.github.io/Scientific-Calculator-JS/",
+    "_blank"
+  );
+});
+ticTac.addEventListener("click", () => {
+  window.open("https://sarthakbhandari.github.io/Tic-Tac-Toe-JS/", "_blank");
+});
